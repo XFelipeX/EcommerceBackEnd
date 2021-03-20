776015bbecd1c80f348a384d820a5e27ab28fa10
ji hye <jihye83@hotmail.com>
2021-03-19 23:11:51 -0300

    Author

---
+++ src/main/java/com/apilibrary/controller/AuthorController.java
@@ -0,0 +1,44 @@
+package com.apilibrary.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apilibrary.model.Author;
+import com.apilibrary.response.SuccessMessage;
+import com.apilibrary.service.AuthorService;
+
+
+import javassist.NotFoundException;
+
+@CrossOrigin
+@RestController
+public class AuthorController {
+	
+	@Autowired
+	private AuthorService serviceAuthor;
+	
+//	@GetMapping("/author")
+//	public Page<Author> listAuthors(Pageable pageable) {
+//		
+//		return serviceAuthor.listAuthors(pageable);
+//	}
+	
+	@GetMapping("/author/{id}")
+	public ResponseEntity<Object> getAuthor(@PathVariable int id) throws NotFoundException {
+		Author author = serviceAuthor.getAuthorById(id);
+		
+		if(author == null) {
+			throw new NotFoundException("Not found for id " + id);
+		}
+		
+		SuccessMessage response = new SuccessMessage(author);
+		
+		return new ResponseEntity<Object>(response, HttpStatus.OK);
+	}
+		
+}
---
+++ src/main/java/com/apilibrary/service/AuthorService.java
@@ -0,0 +1,41 @@
+package com.apilibrary.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.apilibrary.model.Author;
+import com.apilibrary.repository.AuthorRepository;
+
+@Service
+public class AuthorService {
+	@Autowired
+	private AuthorRepository repository;
+	
+	public Author saveAuthor(Author author) {
+		return repository.save(author);
+	}
+	
+	public Page<Author> listAuthors(Pageable pageable){
+		return repository.findAll(pageable);
+	}
+	
+	public Author getAuthorById(int id) {
+		return repository.findById(id).orElse(null);
+	}
+	
+	public String deleteAuthor(int id) {
+		repository.deleteById(id);
+		return "author deleted";
+	}
+	
+	public Author updateAuthor(Author author) {
+		Author existingAuthor = repository.findById(author.getId()).orElse(null);
+		existingAuthor.setNameAuthor(author.getNameAuthor());
+		existingAuthor.setCpf(author.getCpf());
+		
+		return repository.save(existingAuthor);
+	}
+
+}
