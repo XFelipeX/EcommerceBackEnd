233e259d4fd74df77847b695f9fdb3fc82428ba4
Thyagoosrs <thyagoosrs@gmail.com>
2021-05-04 00:41:52 -0300

    Model Item

---
+++ src/main/java/com/apilibrary/model/Item.java
@@ -0,0 +1,32 @@
+package com.apilibrary.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import com.sun.istack.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "item")
+public class Item {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@NotNull @NotBlank
+	private int amount;
+	private int bookId;
+	private int demandId;
+
+}
+
