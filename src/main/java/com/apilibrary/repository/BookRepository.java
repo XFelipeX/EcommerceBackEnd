ede5b54f7033b6d5e47bb19af59d1cffa5c32fbf
Felipe <felipediasamorimpessoa@gmail.com>
2021-05-20 10:51:25 -0300

    client view only active books

--- src/main/java/com/apilibrary/repository/BookRepository.java
+++ src/main/java/com/apilibrary/repository/BookRepository.java
@@ -1,11 +1,17 @@
 package com.apilibrary.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.apilibrary.model.Book;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
-
+	Page<Book> findAllByOrderByIdDesc(Pageable pageable);
+	
+	@Query("SELECT i FROM Book i WHERE i.available =:available")
+	Page<Book> findAllByAvailable(Pageable pageable, int available);
 }
